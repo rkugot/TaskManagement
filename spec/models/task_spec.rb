8a36f85f1be049e836fe17b8eb0401bac896b1fe
rkugot <stream13k@gmail.com>
2017-07-18 07:34:22 +0300

    Add test for task text

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 describe Task do
-  it { should validate_presence_of :text }
+  describe "validations" do
+    it { should validate_presence_of :text }
+  end
+  describe "text" do
+    it "returns the task text" do
+      task = create(:task, text: 'Task1')
+      expect(task.text).to eq 'Task1'
+    end
+  end
 end
