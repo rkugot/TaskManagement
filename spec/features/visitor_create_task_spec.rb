26b45f465cbb1b9ef234c7a9e7c2437acc82d911
rkugot <stream13k@gmail.com>
2017-07-18 09:37:07 +0300

    Add delete test

--- spec/features/visitor_create_task_spec.rb
+++ spec/features/visitor_create_task_spec.rb
@@ -11,4 +11,18 @@ feature 'Create Task' do
     click_button 'Add task'
     expect(page).to have_content 'remove'
   end
+  scenario 'allows delete task' do        
+    visit '/tasks'
+    fill_in 'task[text]', with: 'Task'
+    click_button 'Add task'
+    click_link '(remove)'
+    expect(page).to have_no_content('Task')
+  end
+  scenario 'allows complete task' do        
+    visit '/tasks'
+    fill_in 'task[text]', with: 'Task'
+    click_button 'Add task'
+    find('input[type=checkbox]').click
+    expect(page).to have_content('Task')
+  end
 end
